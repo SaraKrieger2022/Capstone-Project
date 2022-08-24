765e51a1d75ecfa1be0d50bbc5e77a8020e9ccd6
sarakrieger <sara.krieger2022@gmail.com>
2022-08-24 23:37:37 +0200

    Add integration test

---
+++ backend/src/test/java/com/sara/animaladoption/IntegrationTest.java
@@ -0,0 +1,33 @@
+package com.sara.animaladoption;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.RequestBuilder;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import static org.hamcrest.Matchers.hasSize;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+class IntegrationTest {
+
+    @Autowired
+    MockMvc mockMvc;
+
+    @Test
+    void getAllAnimals() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.get("/animals"))
+                .andExpect(status().is(200))
+                .andExpect(jsonPath("$", hasSize(0)))
+                .andExpect(content().json("""
+                        []
+                        """));
+
+
+    }
+
+}
