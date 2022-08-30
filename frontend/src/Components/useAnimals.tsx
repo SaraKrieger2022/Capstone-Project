fd605cfd52dfe8054b8b9d534eacfc1d09a584c2
sarakrieger <sara.krieger2022@gmail.com>
2022-08-30 10:17:33 +0200

    Add new method addAnimal and add new route

--- frontend/src/Components/useAnimals.tsx
+++ frontend/src/Components/useAnimals.tsx
@@ -1,6 +1,7 @@
 import {Animal} from "./Animal";
 import {useEffect, useState} from "react";
 import axios from "axios";
+import {NewAnimal} from "./NewAnimal";
 
 export default function useAnimals() {
     const [animals, setAnimals] = useState<Animal[]>([])
@@ -15,8 +16,32 @@ export default function useAnimals() {
             .then(setAnimals)
     }
 
-    return (
-        {animals}
-    );
+    const addAnimal = (newName: string) => {
+        const newAnimal: NewAnimal = {
+            age: "",
+            breed: "",
+            cats: false,
+            colour: "",
+            gender: "",
+            healthy: false,
+            kids: false,
+            other_dogs: false,
+            size: "",
+            spayed_neutered: false,
+            vaccinated: false,
+            name: newName
+        }
+        return axios.post("/animals", newAnimal)
+            .then((response) => {
+                    return response.data
+                        .then(getAllAnimals)
+                }
+            );
 
+    }
+
+
+    return (
+        {animals, addAnimal}
+    );
 }
