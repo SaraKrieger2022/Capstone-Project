482b49d86f6d46f0b302341e86f5a4bf51253fa9
sarakrieger <sara.krieger2022@gmail.com>
2022-09-01 10:27:10 +0200

    Add flex to AnimalList

--- frontend/src/Components/AnimalList.tsx
+++ frontend/src/Components/AnimalList.tsx
@@ -1,6 +1,7 @@
 import Animal from "./Animal";
 import AnimalCard from "./AnimalCard";
 import 'bootstrap/dist/css/bootstrap.min.css';
+import {ListGroup, ListGroupItem} from "react-bootstrap";
 
 type AnimalListProps = {
     animals: Animal[]
@@ -10,13 +11,13 @@ export default function AnimalList(props: AnimalListProps) {
 
     return (
         <>
-            <ul>
-                {props.animals.map(animal =>
-                    <li key={animal.id}>
-                        <AnimalCard animal={animal}/>
-                    </li>)}
 
-            </ul>
+            <ListGroup horizontal className="d-flex flex-wrap justify-content-evenly bg-transparent gap-5">
+                {props.animals.map(animal =>
+                    <ListGroupItem className="border-0 rounded p-0" key={animal.id}>
+                        <AnimalCard animal={animal}/>
+                    </ListGroupItem>)}
+            </ListGroup>
         </>
     )
 }
