d01919ce9214299e86bb946945edf230c9f9f12e
sarakrieger <sara.krieger2022@gmail.com>
2022-09-15 22:24:38 +0200

    Add React NavLinks to navigation

--- frontend/src/Components/Navigation.tsx
+++ frontend/src/Components/Navigation.tsx
@@ -13,18 +13,13 @@ export default function Navigation() {
             <nav className="navbar-nav mt-4 mb-5">
                 <ul className="nav nav-pills">
                     <li className="nav-item">
-                        <a className="nav-link active" aria-current="page"
-                           href="">Home</a>
-                        <NavLink to={"/"}>Home</NavLink>
+                        <NavLink className="nav-link active" to={"/"}>Home</NavLink>
                     </li>
                     <li className="nav-item box">
-                        <a className="nav-link active" href="">Adopt a dog</a>
-                        <NavLink to={"/animals"}>Adopt a dog</NavLink>
+                        <NavLink className="nav-link active" to={"/animals"}>Adopt a dog</NavLink>
                     </li>
                     <li className="nav-item box">
-                        <a className="nav-link active" href="">Add a
-                            dog</a>
-                        <NavLink to={"/animals/add"}>Add a dog</NavLink>
+                        <NavLink className="nav-link active" to={"/animals/add"}>Add a dog</NavLink>
                     </li>
                 </ul>
             </nav>
